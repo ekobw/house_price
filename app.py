2f1a91e94a15209eac9e3a96a9570069f0bd492f
ekobw <ekobw.py@gmail.com>
2024-02-28 23:03:31 +0700

    commit#

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pickle
 import joblib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import RobustScaler
 import altair as alt
