1f80609d94a17cb44571c35a8f97bdaf9cc70fd0
ekobw <ekobw.py@gmail.com>
2024-02-29 21:33:39 +0700

    commit

--- app_test2.py
+++ app_test2.py
@@ -273,6 +273,7 @@ def run_ml_app():
         features = [0] * 13  # Initialize features with zeros
         city_index = encoding_object.columns.get_loc(encoded_city)
         features[city_index] = 1  # Set the value for the encoded city
+        scaled_values = scale_values(int(kamar_tidur), int(luas_bangunan_m2), int(luas_tanah_m2))
         features.extend(scaled_values)  # Add scaled values
 
         # Predict house price
