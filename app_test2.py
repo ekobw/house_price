7e67d51793bef533975823c4bab5b9dedde96b92
Eko <ekobw.py@gmail.com>
2024-03-01 09:58:46 +0700

    commit

--- app_test2.py
+++ app_test2.py
@@ -228,58 +228,59 @@ def run_ml_app():
 
     # Lakukan one-hot encoding pada data training
     kota_encoded_training = pd.get_dummies(data_training["kota"])
+    kota_encoded_training
 
-    # Muat model
-    with open('./data/final_model.pkl', 'rb') as f:
-        model = pickle.load(f)
+    # # Muat model
+    # with open('./data/final_model.pkl', 'rb') as f:
+    #     model = pickle.load(f)
 
-    # Create a mapping dictionary
-    kota_mapping = {
-        'Jakarta Pusat': 'kota_jakarta_pusat',
-        'Jakarta Selatan': 'kota_jakarta_selatan',
-        'Jakarta Barat': 'kota_jakarta_barat',
-        'Jakarta Utara': 'kota_jakarta_utara',
-        'Jakarta Timur': 'kota_jakarta_timur',
-        'Bogor': 'kota_bogor',
-        'Depok': 'kota_depok',
-        'Bekasi': 'kota_bekasi',
-        'Tangerang': 'kota_tangerang',
-        'Tangerang Selatan': 'kota_tangerang_selatan'
-        }
+    # # Create a mapping dictionary
+    # kota_mapping = {
+    #     'Jakarta Pusat': 'kota_jakarta_pusat',
+    #     'Jakarta Selatan': 'kota_jakarta_selatan',
+    #     'Jakarta Barat': 'kota_jakarta_barat',
+    #     'Jakarta Utara': 'kota_jakarta_utara',
+    #     'Jakarta Timur': 'kota_jakarta_timur',
+    #     'Bogor': 'kota_bogor',
+    #     'Depok': 'kota_depok',
+    #     'Bekasi': 'kota_bekasi',
+    #     'Tangerang': 'kota_tangerang',
+    #     'Tangerang Selatan': 'kota_tangerang_selatan'
+    #     }
 
-    # Buat sidebar untuk input data
-    st.sidebar.header("Masukkan Data Rumah")
-    luas_bangunan_m2 = st.sidebar.number_input("Luas Bangunan (m²)", min_value=0)
-    luas_tanah_m2 = st.sidebar.number_input("Luas Tanah (m²)", min_value=0)
-    kamar_tidur = st.sidebar.number_input("Jumlah Kamar Tidur", min_value=0)
-    kota_input = st.sidebar.selectbox('Kota', list(kota_mapping.keys()))
+    # # Buat sidebar untuk input data
+    # st.sidebar.header("Masukkan Data Rumah")
+    # luas_bangunan_m2 = st.sidebar.number_input("Luas Bangunan (m²)", min_value=0)
+    # luas_tanah_m2 = st.sidebar.number_input("Luas Tanah (m²)", min_value=0)
+    # kamar_tidur = st.sidebar.number_input("Jumlah Kamar Tidur", min_value=0)
+    # kota_input = st.sidebar.selectbox('Kota', list(kota_mapping.keys()))
 
-    # Dapatkan value yang dimapping
-    kota_encoded = kota_mapping[kota_input]
+    # # Dapatkan value yang dimapping
+    # kota_encoded = kota_mapping[kota_input]
 
-    # Lakukan one-hot encoding pada data input pengguna
-    kota_encoded_input = pd.get_dummies([kota_encoded], columns=kota_encoded_training.columns)
+    # # Lakukan one-hot encoding pada data input pengguna
+    # kota_encoded_input = pd.get_dummies([kota_encoded], columns=kota_encoded_training.columns)
 
-    # Gabungkan data kota dengan variabel lain
-    luas_bangunan_series = pd.Series([luas_bangunan_m2])
-    luas_tanah_series = pd.Series([luas_tanah_m2])
-    kamar_tidur_series = pd.Series([kamar_tidur])
+    # # Gabungkan data kota dengan variabel lain
+    # luas_bangunan_series = pd.Series([luas_bangunan_m2])
+    # luas_tanah_series = pd.Series([luas_tanah_m2])
+    # kamar_tidur_series = pd.Series([kamar_tidur])
 
-    data_input = pd.concat([kota_encoded_input, luas_bangunan_series, luas_tanah_series, kamar_tidur_series], axis=1)
+    # data_input = pd.concat([kota_encoded_input, luas_bangunan_series, luas_tanah_series, kamar_tidur_series], axis=1)
     
-    # Define the expected feature names
-    expected_feature_names = ['kota_jakarta_pusat', 'kota_jakarta_selatan', 'kota_jakarta_barat', 'kota_jakarta_utara', 'kota_jakarta_timur', 'kota_bogor', 'kota_depok', 'kota_bekasi', 'kota_tangerang', 'kota_tangerang_selatan', 'luas_bangunan_m2', 'luas_tanah_m2', 'kamar_tidur']
+    # # Define the expected feature names
+    # expected_feature_names = ['kota_jakarta_pusat', 'kota_jakarta_selatan', 'kota_jakarta_barat', 'kota_jakarta_utara', 'kota_jakarta_timur', 'kota_bogor', 'kota_depok', 'kota_bekasi', 'kota_tangerang', 'kota_tangerang_selatan', 'luas_bangunan_m2', 'luas_tanah_m2', 'kamar_tidur']
 
-    # Update the column names of data_input
-    data_input.columns = expected_feature_names
+    # # Update the column names of data_input
+    # data_input.columns = expected_feature_names
 
 
-    # Prediksi harga rumah
-    prediksi = model.predict(data_input)
+    # # Prediksi harga rumah
+    # prediksi = model.predict(data_input)
 
-    # Tampilkan hasil prediksi
-    st.header("Hasil Prediksi Harga Rumah")
-    st.write("Harga Rumah: Rp", str(int(prediksi)))
+    # # Tampilkan hasil prediksi
+    # st.header("Hasil Prediksi Harga Rumah")
+    # st.write("Harga Rumah: Rp", str(int(prediksi)))
 
 if __name__ == '__main__':
     main()
