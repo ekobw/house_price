8ad85b4a1187b5d860e7c0d5ec4730aa40b384f7
ekobw <ekobw.py@gmail.com>
2024-03-11 13:00:49 +0700

    commit

--- app.py
+++ app.py
@@ -111,10 +111,6 @@ def main():
         # Create histograms for each numeric column
         histograms = []
         for col in numeric_columns:
-            # Calculate mean and median
-            mean_val = df[col].mean()
-            median_val = df[col].median()
-
             # Create histogram with mean and median annotations
             histogram = alt.Chart(df).mark_bar().encode(
                 alt.X(col, bin=alt.Bin(maxbins=40)),
@@ -129,30 +125,27 @@ def main():
             mean_rule = alt.Chart(df).mark_rule(color='red').encode(
                 x=f'average({col}):Q',
                 size=alt.value(2),
-                opacity=alt.value(0.7),
-                title=None  # Set title to None
-            ).properties(
-                title=None  # Set title to None
+                opacity=alt.value(0.7)
             )
 
             median_rule = alt.Chart(df).mark_rule(color='blue').encode(
                 x=f'median({col}):Q',
                 size=alt.value(2),
-                opacity=alt.value(0.7),
-                title=None  # Set title to None
-            ).properties(
-                title=None  # Set title to None
+                opacity=alt.value(0.7)
             )
 
             histogram += (mean_rule + median_rule)
 
-            histograms.append(histogram)
+            histograms.append(histogram.configure_axis(
+                labels=False  # disable axis labels
+            ))
 
         # Arrange histograms in a grid layout
         histogram_grid = alt.vconcat(*[alt.hconcat(*histograms[i:i+2]) for i in range(0, len(histograms), 2)])
 
         # Display Altair chart
         st.altair_chart(histogram_grid, use_container_width=True)
+
 
         # # Create histograms for each numeric column
         # histograms = []
